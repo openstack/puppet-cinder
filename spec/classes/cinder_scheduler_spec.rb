8f425918bf969a4439a1941dcc8d292716d1c872
Mathieu Gagné <mgagne@iweb.com>
2013-03-19 16:36:49 -0400

    Make scheduler_driver configurable
    No scheduler_driver will be configured if none is provided.

--- spec/classes/cinder_scheduler_spec.rb
+++ spec/classes/cinder_scheduler_spec.rb
@@ -1,10 +1,72 @@
 require 'spec_helper'
 
 describe 'cinder::scheduler' do
-
-  let :facts do
-    {:osfamily => 'Debian'}
+  
+  describe 'on debian plateforms' do
+    
+    let :facts do
+      { :osfamily => 'Debian' }
+    end
+    
+    describe 'with default parameters' do
+      
+      it { should include_class('cinder::params') }
+      
+      it { should contain_package('cinder-scheduler').with(
+        :name      => 'cinder-scheduler',
+        :ensure    => 'latest',
+        :before    => 'Service[cinder-scheduler]'
+      ) }
+      
+      it { should contain_service('cinder-scheduler').with(
+        :name      => 'cinder-scheduler',
+        :enable    => true,
+        :ensure    => 'running',
+        :require   => 'Package[cinder]',
+        :subscribe => 'File[/etc/cinder/cinder.conf]'
+      ) }
+    end
+    
+    describe 'with parameters' do
+      
+      let :params do
+        { :scheduler_driver => 'cinder.scheduler.filter_scheduler.FilterScheduler',
+          :package_ensure   => 'present'
+        }
+      end
+      
+      it { should contain_cinder_config('DEFAULT/scheduler_driver').with_value('cinder.scheduler.filter_scheduler.FilterScheduler') }
+      it { should contain_package('cinder-scheduler').with_ensure('present') }
+    end
   end
-  it { should contain_package('cinder-scheduler') }
-  it { should contain_service('cinder-scheduler') }
+  
+  
+  describe 'on rhel plateforms' do
+    
+    let :facts do
+      { :osfamily => 'RedHat' }
+    end
+    
+    describe 'with default parameters' do
+      
+      it { should include_class('cinder::params') }
+      
+      it { should contain_service('cinder-scheduler').with(
+        :name      => 'openstack-cinder-scheduler',
+        :enable    => true,
+        :ensure    => 'running',
+        :require   => 'Package[cinder]',
+        :subscribe => 'File[/etc/cinder/cinder.conf]'
+      ) }
+    end
+    
+    describe 'with parameters' do
+      
+      let :params do
+        { :scheduler_driver => 'cinder.scheduler.filter_scheduler.FilterScheduler' }
+      end
+      
+      it { should contain_cinder_config('DEFAULT/scheduler_driver').with_value('cinder.scheduler.filter_scheduler.FilterScheduler') }
+    end
+  end
 end
