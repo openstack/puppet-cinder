7073e22e506a8c8f18c03903b58d41462be0f11a
Joe Topjian <joe@topjian.net>
2013-05-14 14:48:17 -0600

    Adds GlusterFS Driver
    This patch adds a manifest that configures Cinder to use GlusterFS
    as a volume driver.
    
    Change-Id: Ifb874a5c792ac416ec6567bbd70ab291820a04d0

---
+++ manifests/volume/glusterfs.pp
@@ -0,0 +1,59 @@
+#
+# == Class: cinder::volume::glusterfs
+#
+# Configures Cinder to use GlusterFS as a volume driver
+#
+# === Parameters
+#
+# [*glusterfs_shares*]
+#   (required) An array of GlusterFS volume locations.
+#   Must be an array even if there is only one volume.
+#
+# [*glusterfs_disk_util*]
+#   (optional) The utility used to calculate how much free
+#   space is available on each Gluster volume.
+#   Defaults to undef which uses the driver's default of "df".
+#
+# [*glusterfs_sparsed_volumes*]
+#   (optional) Whether or not to use sparse (thin) volumes.
+#   Defaults to undef which uses the driver's default of "true".
+#
+# [*glusterfs_mount_point_base*]
+#   (optional) Where to mount the Gluster volumes.
+#   Defaults to undef which uses the driver's default of "$state_path/mnt".
+#
+# [*glusterfs_shares_config*]
+#   (optional) The config file to store the given $glusterfs_shares.
+#   Defaults to '/etc/cinder/shares.conf'
+#
+# === Examples
+#
+# class { 'cinder::volume::glusterfs':
+#   glusterfs_shares = ['192.168.1.1:/volumes'],
+# }
+#
+class cinder::volume::glusterfs (
+  $glusterfs_shares,
+  $glusterfs_disk_util        = undef,
+  $glusterfs_sparsed_volumes  = undef,
+  $glusterfs_mount_point_base = undef,
+  $glusterfs_shares_config    = "/etc/cinder/shares.conf"
+) {
+
+  $content = join($glusterfs_shares, "\n")
+
+  file {$glusterfs_shares_config:
+    content => "${content}\n",
+    require => Package['cinder'],
+    notify  => Service['cinder-volume']
+  }
+
+  cinder_config {
+    'DEFAULT/volume_driver':        value =>
+      'cinder.volume.drivers.glusterfs.GlusterfsDriver';
+    'DEFAULT/glusterfs_shares_config':    value => $glusterfs_shares_config;
+    'DEFAULT/glusterfs_disk_util':        value => $glusterfs_disk_util;
+    'DEFAULT/glusterfs_sparsed_volumes':  value => $glusterfs_sparsed_volumes;
+    'DEFAULT/glusterfs_mount_point_base': value => $glusterfs_mount_point_base;
+  }
+}
---
+++ spec/classes/cinder_volume_glusterfs_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'cinder::volume::glusterfs' do
+
+  let :params do
+    {
+      :glusterfs_shares           => ['10.10.10.10:/volumes', '10.10.10.11:/volumes'],
+      :glusterfs_shares_config    => '/etc/cinder/other_shares.conf',
+      :glusterfs_disk_util        => 'du',
+      :glusterfs_sparsed_volumes  => 'true',
+      :glusterfs_mount_point_base => '/cinder_mount_point',
+    }
+  end
+
+  describe 'glusterfs volume driver' do
+    it 'configures glusterfs volume driver' do
+      should contain_cinder_config('DEFAULT/volume_driver').with_value(
+        'cinder.volume.drivers.glusterfs.GlusterfsDriver')
+      should contain_cinder_config('DEFAULT/glusterfs_shares_config').with_value(
+        '/etc/cinder/other_shares.conf')
+      should contain_cinder_config('DEFAULT/glusterfs_sparsed_volumes').with_value(
+        'true')
+      should contain_cinder_config('DEFAULT/glusterfs_mount_point_base').with_value(
+        '/cinder_mount_point')
+      should contain_cinder_config('DEFAULT/glusterfs_disk_util').with_value(
+        'du')
+      should contain_file('/etc/cinder/other_shares.conf').with(
+        :content => "10.10.10.10:/volumes\n10.10.10.11:/volumes\n",
+        :require => 'Package[cinder]',
+        :notify  => 'Service[cinder-volume]'
+      )
+    end
+  end
+end
