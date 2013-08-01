b639806155924e6977396f83172281ca4ce26aa5
Logan McNaughton <logan@bacoosta.com>
2013-08-01 17:43:42 +0000

    Setup test volume fails without lvm2
    On Ubuntu, the lvm2 package isn't installed by default.
    Setup test volume fails without the lvm2 package installed first.
    
    Change-Id: I1c453102d3554fd4853e626af5daa74367ced88b

--- spec/classes/cinder_setup_test_volume_spec.rb
+++ spec/classes/cinder_setup_test_volume_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe 'cinder::setup_test_volume' do
 
+  it { should contain_package('lvm2').with(
+        :ensure => 'present'
+      ) }
+
   it 'should contain volume creation execs' do
     should contain_exec('/bin/dd if=/dev/zero of=cinder-volumes bs=1 count=0 seek=4G')
     should contain_exec('/sbin/losetup /dev/loop2 cinder-volumes')
