9bd451830d124703ab4b50dcab10e0a697c85ad6
Francois Deppierraz <francois.deppierraz@nimag.net>
2013-08-27 15:50:03 +0200

    Add syslog support to cinder module
    Change-Id: I6e974fddfb77901827ca06bab18250529f970bae

--- spec/classes/cinder_spec.rb
+++ spec/classes/cinder_spec.rb
@@ -136,4 +136,35 @@ describe 'cinder' do
 
   end
 
+  describe 'with syslog disabled' do
+    let :params do
+      req_params
+    end
+
+    it { should contain_cinder_config('DEFAULT/use_syslog').with_value(false) }
+  end
+
+  describe 'with syslog enabled' do
+    let :params do
+      req_params.merge({
+        :use_syslog   => 'true',
+      })
+    end
+
+    it { should contain_cinder_config('DEFAULT/use_syslog').with_value(true) }
+    it { should contain_cinder_config('DEFAULT/syslog_log_facility').with_value('LOG_USER') }
+  end
+
+  describe 'with syslog enabled and custom settings' do
+    let :params do
+      req_params.merge({
+        :use_syslog   => 'true',
+        :log_facility => 'LOG_LOCAL0'
+     })
+    end
+
+    it { should contain_cinder_config('DEFAULT/use_syslog').with_value(true) }
+    it { should contain_cinder_config('DEFAULT/syslog_log_facility').with_value('LOG_LOCAL0') }
+  end
+
 end
