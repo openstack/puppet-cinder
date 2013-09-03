3115944ae170da7b64afa334a758f82a2cb85478
Mathieu Gagné <mgagne@iweb.com>
2013-09-03 09:51:10 -0400

    Add ability to configure control_exchange
    Add control_exchange parameter with default to 'openstack'.
    
    The control_exchange configuration defaults to 'openstack' in Cinder.
    
    Fixes: bug #1220272
    Change-Id: Ic15e471a745568e5183ad9eedba92c213026309e
    (cherry picked from commit 15e32b1e4ab513fbe980e84d6585be42385c0ca7)

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,6 +17,7 @@ class cinder (
   $sql_connection,
   $sql_idle_timeout            = '3600',
   $rpc_backend                 = 'cinder.openstack.common.rpc.impl_kombu',
+  $control_exchange            = 'openstack',
   $rabbit_host                 = '127.0.0.1',
   $rabbit_port                 = 5672,
   $rabbit_hosts                = false,
@@ -85,6 +86,7 @@ class cinder (
       'DEFAULT/rabbit_password':     value => $rabbit_password, secret => true;
       'DEFAULT/rabbit_userid':       value => $rabbit_userid;
       'DEFAULT/rabbit_virtual_host': value => $rabbit_virtual_host;
+      'DEFAULT/control_exchange':    value => $control_exchange;
     }
 
     if $rabbit_hosts {
