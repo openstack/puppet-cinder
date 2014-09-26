82d1b3199a8c84d25659e6f6ba8de07b42435f83
Yanis Guenane <yanis.guenane@enovance.com>
2014-09-26 19:04:48 -0400

    policy.json: Allow one to manage them from the puppet module
    This commit allow a deployer to manage the policies via this module
    It relies on augeas to change only the policy needed. The init takes
    a hash of policies and apply them.
    
    Change-Id: I398fca298330b8cc5d5d7dfbf6ebed067b34f9d7

---
+++ manifests/policy.pp
@@ -0,0 +1,28 @@
+# == Class: cinder::policy
+#
+# Configure the cinder policies
+#
+# === Parameters
+#
+# [*policies*]
+#   (optional) Set of policies to configure for cinder
+#   Example : { 'cinder-context_is_admin' => {'context_is_admin' => 'true'}, 'cinder-default' => {'default' => 'rule:admin_or_owner'} }
+#   Defaults to empty hash.
+#
+# [*policy_path*]
+#   (optional) Path to the cinder policy.json file
+#   Defaults to /etc/cinder/policy.json
+#
+class cinder::policy (
+  $policies    = {},
+  $policy_path = '/etc/cinder/policy.json',
+) {
+
+  Openstacklib::Policy::Base {
+    file_path => $policy_path,
+  }
+  class { 'openstacklib::policy' :
+    policies => $policies,
+  }
+
+}
