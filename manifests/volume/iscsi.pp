0f67be560c7b95d80555fd4fd4cd794274bf7d8a
Jiri Stransky <jistr@redhat.com>
2014-10-06 14:04:24 +0200

    Set volume_driver for iscsi backend explicitly
    Setting the volume_driver option explicitly even for iscsi backend is
    desirable, because if the user would switch from e.g. an nfs backend
    to iscsi, the volume_driver config option would not be changed during
    the Puppet run and still point to the nfs driver.
    
    Defaults to LVMISCSIDriver, can be overriden to LVMISERDriver.
    
    Change-Id: I860508ff92fce4add21c50e684e1717fc4f16622

--- manifests/volume/iscsi.pp
+++ manifests/volume/iscsi.pp
@@ -1,6 +1,7 @@
 #
 class cinder::volume::iscsi (
   $iscsi_ip_address,
+  $volume_driver     = 'cinder.volume.drivers.lvm.LVMISCSIDriver',
   $volume_group      = 'cinder-volumes',
   $iscsi_helper      = $::cinder::params::iscsi_helper,
 ) {
@@ -9,6 +10,7 @@ class cinder::volume::iscsi (
 
   cinder::backend::iscsi { 'DEFAULT':
     iscsi_ip_address   => $iscsi_ip_address,
+    volume_driver      => $volume_driver,
     volume_group       => $volume_group,
     iscsi_helper       => $iscsi_helper
   }
