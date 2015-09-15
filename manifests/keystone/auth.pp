bdf24ea401ab718dd47b8c8cc9a1efd5d3fbc418
Emilien Macchi <emilien@redhat.com>
2015-09-15 18:48:01 -0400

    Make sure cinder is created before creating Cinder::Type
    When creating Cinder::Type resources, we need to make sure Keystone
    resources for Cinder are created, because they'll be used by
    Cinder::Type to create a volume type.
    
    This patch is useful when Cinder::Type is declared on the same node as
    Cinder API & Keystone server.
    
    Change-Id: Ia823d80a4eb384ac51d8b1711e8664a3d29247f7

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -311,6 +311,7 @@ class cinder::keystone::auth (
 
   if $configure_user_role {
     Keystone_user_role["${auth_name}@${tenant}"] ~> Service <| name == 'cinder-api' |>
+    Keystone_user_role["${auth_name}@${tenant}"] -> Cinder::Type <| |>
   }
 
 }
