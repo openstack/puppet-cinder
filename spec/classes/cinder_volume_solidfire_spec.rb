694e2edf41d5600883ed97de736eedc407b708f7
cdelatte <craig.delatte@twcable.com>
2016-01-26 10:39:43 -0500

    Add glance caching options for soldifire
    This allows the user to set the options for solidfire to cache glance
    images for subsequent glance calls to the same image
    
    Change-Id: I53697d58b58d33657a0ca2bb0eaf0ba7a770897d

--- spec/classes/cinder_volume_solidfire_spec.rb
+++ spec/classes/cinder_volume_solidfire_spec.rb
@@ -19,6 +19,8 @@ describe 'cinder::volume::solidfire' do
       is_expected.to contain_cinder_config('DEFAULT/san_ip').with_value('127.0.0.2')
       is_expected.to contain_cinder_config('DEFAULT/san_login').with_value('solidfire')
       is_expected.to contain_cinder_config('DEFAULT/san_password').with_value('password')
+      is_expected.to contain_cinder_config('DEFAULT/sf_template_account_name').with_value('openstack-vtemplate')
+      is_expected.to contain_cinder_config('DEFAULT/sf_allow_template_caching').with_value('false')
     end
 
     it 'marks san_password as secret' do
