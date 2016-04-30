eaa0b0ddf9548f17bc18e21a6b6c1bed0b268f45
ZhongShengping <chdzsp@163.com>
2016-04-30 09:59:07 +0800

    Test multiple operating systems for cinder::db::postgresql
    Change-Id: I80f32b3bd26a9c3a71a46779ea1b317d006f0a3b

--- spec/classes/cinder_db_postgresql_spec.rb
+++ spec/classes/cinder_db_postgresql_spec.rb
@@ -2,21 +2,13 @@ require 'spec_helper'
 
 describe 'cinder::db::postgresql' do
 
-  let :req_params do
-    { :password => 'pw' }
-  end
+  shared_examples_for 'cinder::db::postgresql' do
+    let :req_params do
+      { :password => 'pw' }
+    end
 
-  let :pre_condition do
-    'include postgresql::server'
-  end
-
-  context 'on a RedHat osfamily' do
-    let :facts do
-      {
-        :osfamily                 => 'RedHat',
-        :operatingsystemrelease   => '7.0',
-        :concat_basedir => '/var/lib/puppet/concat'
-      }
+    let :pre_condition do
+      'include postgresql::server'
     end
 
     context 'with only required parameters' do
@@ -32,27 +24,19 @@ describe 'cinder::db::postgresql' do
 
   end
 
-  context 'on a Debian osfamily' do
-    let :facts do
-      {
-        :operatingsystemrelease => '7.8',
-        :operatingsystem        => 'Debian',
-        :osfamily               => 'Debian',
-        :concat_basedir => '/var/lib/puppet/concat'
-      }
-    end
-
-    context 'with only required parameters' do
-      let :params do
-        req_params
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({
+          :processorcount => 8,
+          :concat_basedir => '/var/lib/puppet/concat'
+        }))
       end
 
-      it { is_expected.to contain_postgresql__server__db('cinder').with(
-        :user     => 'cinder',
-        :password => 'md506736c3030793e09882cc536063d433f'
-      )}
+      it_configures 'cinder::db::postgresql'
     end
-
   end
 
 end
