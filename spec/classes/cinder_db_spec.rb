fffd82deecff291fae7fe09f895d4de0368f0e7c
Giulio Fidente <gfidente@redhat.com>
2016-05-09 13:37:41 +0200

    Add support for db_max_retries param
    The db_max_retries parameter regulates the number of reconnection
    attempts performed after an error raised rather than at startup.
    
    Change-Id: I197e5de614d1b69e335e0f910cc1262bd7c85ed8
    Closes-Bug: 1579718

--- spec/classes/cinder_db_spec.rb
+++ spec/classes/cinder_db_spec.rb
@@ -6,6 +6,7 @@ describe 'cinder::db' do
 
     context 'with default parameters' do
 
+      it { is_expected.to contain_cinder_config('database/db_max_retries').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_cinder_config('database/connection').with_value('sqlite:////var/lib/cinder/cinder.sqlite').with_secret(true) }
       it { is_expected.to contain_cinder_config('database/idle_timeout').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_cinder_config('database/min_pool_size').with_value('<SERVICE DEFAULT>') }
@@ -16,13 +17,15 @@ describe 'cinder::db' do
 
     context 'with specific parameters' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://cinder:cinder@localhost/cinder',
+        { :database_db_max_retries => '-1',
+          :database_connection     => 'mysql+pymysql://cinder:cinder@localhost/cinder',
           :database_idle_timeout   => '3601',
           :database_min_pool_size  => '2',
           :database_max_retries    => '11',
           :database_retry_interval => '11', }
       end
 
+      it { is_expected.to contain_cinder_config('database/db_max_retries').with_value('-1') }
       it { is_expected.to contain_cinder_config('database/connection').with_value('mysql+pymysql://cinder:cinder@localhost/cinder').with_secret(true) }
       it { is_expected.to contain_cinder_config('database/idle_timeout').with_value('3601') }
       it { is_expected.to contain_cinder_config('database/min_pool_size').with_value('2') }
