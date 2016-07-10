cb8e2d576e039a238d5bc356384cab8e001d0c71
Iury Gregory Melo Ferreira <iurygregory@gmail.com>
2016-07-10 16:47:41 -0300

    Changes in EMC VNX
    - added configuration for hardcoded parameters
    - added new parameters according to docs [0]
    - update tests
    - added release note
    
    [0] http://docs.openstack.org/mitaka/config-reference/block-storage/drivers/emc-vnx-driver.html
    
    Change-Id: Ia97ef935a71c3ccf41c54471597be89a5486fb0b

--- spec/classes/cinder_volume_emc_vnx_spec.rb
+++ spec/classes/cinder_volume_emc_vnx_spec.rb
@@ -12,7 +12,7 @@ describe 'cinder::volume::emc_vnx' do
   end
 
   let :facts do
-    {:osfamily => 'Redhat' }
+    OSDefaults.get_facts({:osfamily => 'Redhat' })
   end
 
   let :params do
@@ -27,8 +27,31 @@ describe 'cinder::volume::emc_vnx' do
       is_expected.to contain_cinder_config('DEFAULT/san_password').with_value('password')
       is_expected.to contain_cinder_config('DEFAULT/iscsi_ip_address').with_value('127.0.0.3')
       is_expected.to contain_cinder_config('DEFAULT/storage_vnx_pool_name').with_value('emc-storage-pool')
+      is_expected.to contain_cinder_config('DEFAULT/initiator_auto_registration').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_cinder_config('DEFAULT/storage_vnx_authentication_type').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_cinder_config('DEFAULT/storage_vnx_security_file_dir').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_cinder_config('DEFAULT/naviseccli_path').with_value('<SERVICE DEFAULT>')
     end
   end
+
+  describe 'emc vnx backend overriding some parameters' do
+    before :each do
+      req_params.merge!({
+       :initiator_auto_registration   => true,
+       :storage_vnx_auth_type         => 'global',
+       :storage_vnx_security_file_dir => '/etc/secfile/array1',
+       :naviseccli_path               => '/opt/Navisphere/bin/naviseccli',
+      })
+    end
+
+    it 'configure emc vnx volume driver' do
+      is_expected.to contain_cinder_config('DEFAULT/initiator_auto_registration').with_value(req_params[:initiator_auto_registration])
+      is_expected.to contain_cinder_config('DEFAULT/storage_vnx_authentication_type').with_value(req_params[:storage_vnx_auth_type])
+      is_expected.to contain_cinder_config('DEFAULT/storage_vnx_security_file_dir').with_value(req_params[:storage_vnx_security_file_dir])
+      is_expected.to contain_cinder_config('DEFAULT/naviseccli_path').with_value(req_params[:naviseccli_path])
+    end
+  end
+
 
   describe 'emc vnx volume driver with additional configuration' do
     before :each do
