724cbdf27fda0ac4d6919e16dbedc81ff8cdf432
Dimitri Savineau <dsavinea@redhat.com>
2016-09-14 11:03:22 -0400

    Nexenta: Update parameters
    - Add REST port with default 8457.
    - Add volume driver.
    - Update volume driver to iSCSI driver.
    - Blocksize should be an integer.
    
    Change-Id: Ib8ece62f9fb5ff38ccdcf1e19fd84375b8595234

--- manifests/volume/nexenta.pp
+++ manifests/volume/nexenta.pp
@@ -14,19 +14,32 @@
 #   (required) IP address of Nexenta SA.
 #
 # [*nexenta_volume*]
-#   (optional) Pool on SA that will hold all volumes. Defaults to 'cinder'.
+#   (optional) Pool on SA that will hold all volumes.
+#   Defaults to 'cinder'.
 #
 # [*nexenta_target_prefix*]
-#   (optional) IQN prefix for iSCSI targets. Defaults to 'iqn:'.
+#   (optional) IQN prefix for iSCSI targets.
+#   Defaults to 'iqn:'.
 #
 # [*nexenta_target_group_prefix*]
-#   (optional) Prefix for iSCSI target groups on SA. Defaults to 'cinder/'.
+#   (optional) Prefix for iSCSI target groups on SA.
+#   Defaults to 'cinder/'.
 #
 # [*nexenta_blocksize*]
-#   (optional) Block size for volumes. Defaults to '8k'.
+#   (optional) Block size for volumes.
+#   Defaults to '8k'.
 #
 # [*nexenta_sparse*]
-#   (optional) Flag to create sparse volumes. Defaults to true.
+#   (optional) Flag to create sparse volumes.
+#   Defaults to true.
+#
+# [*nexenta_rest_port*]
+#   (optional) HTTP port for REST API.
+#   Defaults to '8457'.
+#
+# [*volume_driver*]
+#   (required) Nexenta driver to use.
+#   Defaults to: 'cinder.volume.drivers.nexenta.iscsi.NexentaISCSIDriver'.
 #
 # [*extra_options*]
 #   (optional) Hash of extra options to pass to the backend stanza
@@ -41,8 +54,10 @@ class cinder::volume::nexenta (
   $nexenta_volume               = 'cinder',
   $nexenta_target_prefix        = 'iqn:',
   $nexenta_target_group_prefix  = 'cinder/',
-  $nexenta_blocksize            = '8k',
+  $nexenta_blocksize            = '8192',
   $nexenta_sparse               = true,
+  $nexenta_rest_port            = '8457',
+  $volume_driver                = 'cinder.volume.drivers.nexenta.iscsi.NexentaISCSIDriver',
   $extra_options                = {},
 ) {
 
@@ -60,6 +75,8 @@ cinder::backend::nexenta instead.')
     nexenta_target_group_prefix => $nexenta_target_group_prefix,
     nexenta_blocksize           => $nexenta_blocksize,
     nexenta_sparse              => $nexenta_sparse,
+    nexenta_rest_port           => $nexenta_rest_port,
+    volume_driver               => $volume_driver,
     extra_options               => $extra_options,
   }
 }
