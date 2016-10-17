68f5a1daf832127d661d01ef98d7a05fd23fd280
Trygve Vea <trygve.vea@gmail.com>
2016-10-17 10:44:25 +0200

    Deprecate iscsi_ip_address, as it is not used
    The EMC VNX volume drivers does not use iscsi_ip_address for
    anything.
    
    The iscsi_ip_address parameter is deprecated, has no effect
    and will be removed in future release.
    
    Change-Id: Ib8705171c4750d2039ff97a616eae9f309db08a0

--- spec/defines/cinder_backend_emc_vnx_spec.rb
+++ spec/defines/cinder_backend_emc_vnx_spec.rb
@@ -8,7 +8,6 @@ describe 'cinder::backend::emc_vnx' do
       :san_ip                => '127.0.0.2',
       :san_login             => 'emc',
       :san_password          => 'password',
-      :iscsi_ip_address      => '127.0.0.3',
       :storage_vnx_pool_name => 'emc-storage-pool'
     }
   end
@@ -27,13 +26,11 @@ describe 'cinder::backend::emc_vnx' do
       is_expected.to contain_cinder_config('emc/san_ip').with_value('127.0.0.2')
       is_expected.to contain_cinder_config('emc/san_login').with_value('emc')
       is_expected.to contain_cinder_config('emc/san_password').with_value('password')
-      is_expected.to contain_cinder_config('emc/iscsi_ip_address').with_value('127.0.0.3')
       is_expected.to contain_cinder_config('emc/storage_vnx_pool_name').with_value('emc-storage-pool')
       is_expected.to contain_cinder_config('emc/initiator_auto_registration').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_cinder_config('emc/storage_vnx_authentication_type').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_cinder_config('emc/storage_vnx_security_file_dir').with_value('<SERVICE DEFAULT>')
       is_expected.to contain_cinder_config('emc/naviseccli_path').with_value('<SERVICE DEFAULT>')
-
     end
   end
 
