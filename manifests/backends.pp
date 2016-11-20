7cca9751db58f08f836b1c9b20da3f8d1bbbf980
ZhongShengping <chdzsp@163.com>
2016-11-20 19:44:01 +0800

    Fix annotation for the enabled_backends parameter
    Change-Id: I076cd524ffbc3bd61bc114f865be9d03f447615e

--- manifests/backends.pp
+++ manifests/backends.pp
@@ -6,12 +6,13 @@
 #
 # [*enabled_backends*]
 #   (Required) a list of ini sections to enable.
-#   This should contain names used in ceph::backend::* resources.
+#   This should contain names used in cinder::backend::* resources.
 #   Example: ['volume1', 'volume2', 'sata3']
+#   Defaults to undef
 #
 # Author: Andrew Woodward <awoodward@mirantis.com>
 class cinder::backends (
-  $enabled_backends    = undef,
+  $enabled_backends = undef,
 ) {
 
   include ::cinder::deps
