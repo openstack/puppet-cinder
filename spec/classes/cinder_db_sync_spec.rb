c9ad821c93c08f9b2b07312167bb6000d7ac0308
ZhongShengping <chdzsp@163.com>
2016-12-01 14:58:39 +0800

    Fix spec test for sync
    Change-Id: Ia3f5dd3abd204ac1db37e5b42b9dd533102f0845

--- spec/classes/cinder_db_sync_spec.rb
+++ spec/classes/cinder_db_sync_spec.rb
@@ -10,7 +10,13 @@ describe 'cinder::db::sync' do
         :user        => 'cinder',
         :path        => '/usr/bin',
         :refreshonly => 'true',
-        :logoutput   => 'on_failure'
+        :try_sleep   => 5,
+        :tries       => 10,
+        :logoutput   => 'on_failure',
+        :subscribe   => ['Anchor[cinder::install::end]',
+                         'Anchor[cinder::config::end]',
+                         'Anchor[cinder::dbsync::begin]'],
+        :notify      => 'Anchor[cinder::dbsync::end]',
       )
     end
 
@@ -27,7 +33,13 @@ describe 'cinder::db::sync' do
           :user        => 'cinder',
           :path        => '/usr/bin',
           :refreshonly => 'true',
-          :logoutput   => 'on_failure'
+          :try_sleep   => 5,
+          :tries       => 10,
+          :logoutput   => 'on_failure',
+          :subscribe   => ['Anchor[cinder::install::end]',
+                         'Anchor[cinder::config::end]',
+                         'Anchor[cinder::dbsync::begin]'],
+          :notify      => 'Anchor[cinder::dbsync::end]',
         )
       }
     end
