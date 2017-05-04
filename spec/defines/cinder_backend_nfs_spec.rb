699bc9ce333c9be5b6dc030e1dad6c959841ba9e
Alan Bishop <abishop@redhat.com>
2017-05-04 12:09:14 -0400

    Add support for Cinder "NAS secure" driver params
    Add ability to set Cinder's nas_secure_file_operations and
    nas_secure_file_permissions driver parameters. Two sets of identically
    named parameters are implemented by Cinder's NFS and NetApp back end
    drivers.
    
    The ability to control these parameters is crucial for supporting deployments
    that require non-default values.
    
    Partial-Bug: #1688332
    Change-Id: Id92cfd4190de8687d4731cf301f2df0bde1ba7d9
    (cherry picked from commit 02e29ba67dbeceac3d9027686e86fbf9aa3ac51b)

--- spec/defines/cinder_backend_nfs_spec.rb
+++ spec/defines/cinder_backend_nfs_spec.rb
@@ -6,15 +6,17 @@ describe 'cinder::backend::nfs' do
 
   let :params do
     {
-      :nfs_servers          => ['10.10.10.10:/shares', '10.10.10.10:/shares2'],
-      :nfs_mount_attempts   => '4',
-      :nfs_mount_options    => 'vers=3',
-      :nfs_shares_config    => '/etc/cinder/other_shares.conf',
-      :nfs_disk_util        => 'du',
-      :nfs_sparsed_volumes  => true,
-      :nfs_mount_point_base => '/cinder_mount_point',
-      :nfs_used_ratio       => '0.7',
-      :nfs_oversub_ratio    => '0.9'
+      :nfs_servers                 => ['10.10.10.10:/shares', '10.10.10.10:/shares2'],
+      :nfs_mount_attempts          => '4',
+      :nfs_mount_options           => 'vers=3',
+      :nfs_shares_config           => '/etc/cinder/other_shares.conf',
+      :nfs_disk_util               => 'du',
+      :nfs_sparsed_volumes         => true,
+      :nfs_mount_point_base        => '/cinder_mount_point',
+      :nfs_used_ratio              => '0.7',
+      :nfs_oversub_ratio           => '0.9',
+      :nas_secure_file_operations  => 'auto',
+      :nas_secure_file_permissions => 'false',
     }
   end
 
@@ -41,6 +43,10 @@ describe 'cinder::backend::nfs' do
         '0.7')
       is_expected.to contain_cinder_config('hippo/nfs_oversub_ratio').with_value(
         '0.9')
+      is_expected.to contain_cinder_config('hippo/nas_secure_file_operations').with_value(
+        'auto')
+      is_expected.to contain_cinder_config('hippo/nas_secure_file_permissions').with_value(
+        'false')
       is_expected.to contain_file('/etc/cinder/other_shares.conf').with(
         :content => "10.10.10.10:/shares\n10.10.10.10:/shares2",
         :require => 'Anchor[cinder::install::end]',
