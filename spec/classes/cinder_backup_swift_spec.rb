253e35e4e98d9ac1827c27d24f460b5b525dc051
Xingchao Yu <yuxcer@gmail.com>
2017-05-11 10:07:37 +0800

    Fix spec tests for cinder::backup::swift
    Change-Id: Iddd759c9223711f7ded837e05c1254be33998bd9

--- spec/classes/cinder_backup_swift_spec.rb
+++ spec/classes/cinder_backup_swift_spec.rb
@@ -29,9 +29,9 @@ describe 'cinder::backup::swift' do
       :backup_swift_object_size     => '<SERVICE DEFAULT>',
       :backup_swift_retry_attempts  => '<SERVICE DEFAULT>',
       :backup_swift_retry_backoff   => '<SERVICE DEFAULT>',
-      :backup_swift_user_domain     => '<SERVICE_DEFAULT>',
-      :backup_swift_project_domain  => '<SERVICE_DEFAULT>',
-      :backup_swift_project         => '<SERVICE_DEFAULT>',
+      :backup_swift_user_domain     => '<SERVICE DEFAULT>',
+      :backup_swift_project_domain  => '<SERVICE DEFAULT>',
+      :backup_swift_project         => '<SERVICE DEFAULT>',
       :backup_compression_algorithm => '<SERVICE DEFAULT>' }
   end
 
@@ -52,9 +52,9 @@ describe 'cinder::backup::swift' do
       is_expected.to contain_cinder_config('DEFAULT/backup_swift_object_size').with_value(p[:backup_swift_object_size])
       is_expected.to contain_cinder_config('DEFAULT/backup_swift_retry_attempts').with_value(p[:backup_swift_retry_attempts])
       is_expected.to contain_cinder_config('DEFAULT/backup_swift_retry_backoff').with_value(p[:backup_swift_retry_backoff])
-      is_expected.to contain_cinder_config('DEFAULT/backup_swift_user_domain').with_value(p[:backup_compression_algorithm])
-      is_expected.to contain_cinder_config('DEFAULT/backup_swift_project_domain').with_value(p[:backup_compression_algorithm])
-      is_expected.to contain_cinder_config('DEFAULT/backup_swift_project').with_value(p[:backup_compression_algorithm])
+      is_expected.to contain_cinder_config('DEFAULT/backup_swift_user_domain').with_value(p[:backup_swift_user_domain])
+      is_expected.to contain_cinder_config('DEFAULT/backup_swift_project_domain').with_value(p[:backup_swift_project_domain])
+      is_expected.to contain_cinder_config('DEFAULT/backup_swift_project').with_value(p[:backup_swift_project])
       is_expected.to contain_cinder_config('DEFAULT/backup_compression_algorithm').with_value(p[:backup_compression_algorithm])
     end
 
