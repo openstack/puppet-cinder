2101fad88b5fb511d0d08c22a6b1fd42649d0dfc
rajinir <rajini_ram@dell.com>
2017-07-19 13:51:19 -0500

    Add support for Dell EMC VMAX ISCSI backend
    This change adds a new define for cinder::backend::dellemc_vmax_iscsi,
    allowing users to create multiple backend definitions within their puppet
    manifests for use with Cinder.
    
    Change-Id: I59f2c6c28397d3cae283eca8f5d6c414d768076f
    Implements: blueprint support-dellemc-vmax-iscsi

---
+++ manifests/backend/dellemc_vmax_iscsi.pp
@@ -0,0 +1,58 @@
+#
+# == Define: cinder::backend::dellemc_vmax_iscsi
+#
+# Setup Cinder to use the Dell EMC VMAX ISCSI Driver
+# Compatible for multiple backends
+#
+# == Parameters
+#
+# [*volume_backend_name*]
+#   (optional) Allows for the volume_backend_name to be separate of $name.
+#   Defaults to: $name
+#
+# [*cinder_emc_config_file*]
+#   (required) File path of Dell EMC VMAX ISCSI specific configuration file.
+#
+# [*volume_driver*]
+#   (optional) The Dell EMC VMAX ISCSI Driver
+#   Defaults to cinder.volume.drivers.emc.emc_vmax_fc.EMCVMAXISCSIDriver
+#
+# [*manage_volume_type*]
+#   (Optional) Whether or not manage Cinder Volume type.
+#   If set to true, a Cinde Volume type will be created
+#   with volume_backend_name=$volume_backend_name key/value.
+#   Defaults to false.
+#
+# [*extra_options*]
+#   (optional) Hash of extra options to pass to the backend stanza
+#   Defaults to: {}
+#   Example :
+#     { 'dellemc_vmax_iscsi_backend/param1' => { 'value' => value1 } }#
+#
+define cinder::backend::dellemc_vmax_iscsi (
+  $cinder_emc_config_file,
+  $volume_backend_name           = $name,
+  $extra_options                 = {},
+  $volume_driver                 = 'cinder.volume.drivers.emc.emc_vmax_iscsi.EMCVMAXISCSIDriver',
+  $manage_volume_type            = false,
+) {
+
+  include ::cinder::deps
+
+
+  cinder_config {
+    "${name}/volume_backend_name":             value => $volume_backend_name;
+    "${name}/volume_driver":                   value => $volume_driver;
+    "${name}/cinder_emc_config_file":          value => $cinder_emc_config_file;
+  }
+
+  if $manage_volume_type {
+    cinder_type { $volume_backend_name:
+      ensure     => present,
+      properties => ["volume_backend_name=${volume_backend_name}"],
+    }
+  }
+
+  create_resources('cinder_config', $extra_options)
+
+}
