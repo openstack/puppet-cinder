c77755797a15e77b23e442a51f3b41fbc9752d07
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2017-09-04 16:49:47 +0200

    add parameter to overwrite/add wsgi process options
    Add parameter to apache_wsgi to allow overwrite
    and/or add additional wsgi process options.
    
    This possibility was added to openstacklib
    with Change-Id: I41914ce3361988d5db1695f09d21209772fdf548
    
    Change-Id: I0b1b5b9ec242ff01b4b42a80a723be0308c4bc81

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -66,6 +66,14 @@
 #     apache::vhost ssl parameters.
 #     Optional. Default to apache::vhost 'ssl_*' defaults.
 #
+#   [*custom_wsgi_process_options*]
+#     (optional) gives you the oportunity to add custom process options or to
+#     overwrite the default options for the WSGI main process.
+#     eg. to use a virtual python environment for the WSGI process
+#     you could set it to:
+#     { python-path => '/my/python/virtualenv' }
+#     Defaults to {}
+#
 # == Dependencies
 #
 #   requires Class['apache'] & Class['cinder']
@@ -77,22 +85,23 @@
 #   class { 'cinder::wsgi::apache': }
 #
 class cinder::wsgi::apache (
-  $servername                 = $::fqdn,
-  $port                       = 8776,
-  $bind_host                  = undef,
-  $path                       = '/',
-  $ssl                        = true,
-  $workers                    = 1,
-  $ssl_cert                   = undef,
-  $ssl_key                    = undef,
-  $ssl_chain                  = undef,
-  $ssl_ca                     = undef,
-  $ssl_crl_path               = undef,
-  $ssl_crl                    = undef,
-  $ssl_certs_dir              = undef,
-  $wsgi_process_display_name  = undef,
-  $threads                    = $::os_workers,
-  $priority                   = '10',
+  $servername                  = $::fqdn,
+  $port                        = 8776,
+  $bind_host                   = undef,
+  $path                        = '/',
+  $ssl                         = true,
+  $workers                     = 1,
+  $ssl_cert                    = undef,
+  $ssl_key                     = undef,
+  $ssl_chain                   = undef,
+  $ssl_ca                      = undef,
+  $ssl_crl_path                = undef,
+  $ssl_crl                     = undef,
+  $ssl_certs_dir               = undef,
+  $wsgi_process_display_name   = undef,
+  $threads                     = $::os_workers,
+  $priority                    = '10',
+  $custom_wsgi_process_options = {},
 ) {
 
   include ::cinder::deps
@@ -104,29 +113,30 @@ class cinder::wsgi::apache (
   }
 
   ::openstacklib::wsgi::apache { 'cinder_wsgi':
-    bind_host                 => $bind_host,
-    bind_port                 => $port,
-    group                     => 'cinder',
-    path                      => $path,
-    priority                  => $priority,
-    servername                => $servername,
-    ssl                       => $ssl,
-    ssl_ca                    => $ssl_ca,
-    ssl_cert                  => $ssl_cert,
-    ssl_certs_dir             => $ssl_certs_dir,
-    ssl_chain                 => $ssl_chain,
-    ssl_crl                   => $ssl_crl,
-    ssl_crl_path              => $ssl_crl_path,
-    ssl_key                   => $ssl_key,
-    threads                   => $threads,
-    user                      => 'cinder',
-    workers                   => $workers,
-    wsgi_daemon_process       => 'cinder-api',
-    wsgi_process_display_name => $wsgi_process_display_name,
-    wsgi_process_group        => 'cinder-api',
-    wsgi_script_dir           => $::cinder::params::cinder_wsgi_script_path,
-    wsgi_script_file          => 'cinder-api',
-    wsgi_script_source        => $::cinder::params::cinder_wsgi_script_source,
-    require                   => Anchor['cinder::install::end'],
+    bind_host                   => $bind_host,
+    bind_port                   => $port,
+    group                       => 'cinder',
+    path                        => $path,
+    priority                    => $priority,
+    servername                  => $servername,
+    ssl                         => $ssl,
+    ssl_ca                      => $ssl_ca,
+    ssl_cert                    => $ssl_cert,
+    ssl_certs_dir               => $ssl_certs_dir,
+    ssl_chain                   => $ssl_chain,
+    ssl_crl                     => $ssl_crl,
+    ssl_crl_path                => $ssl_crl_path,
+    ssl_key                     => $ssl_key,
+    threads                     => $threads,
+    user                        => 'cinder',
+    workers                     => $workers,
+    wsgi_daemon_process         => 'cinder-api',
+    wsgi_process_display_name   => $wsgi_process_display_name,
+    wsgi_process_group          => 'cinder-api',
+    wsgi_script_dir             => $::cinder::params::cinder_wsgi_script_path,
+    wsgi_script_file            => 'cinder-api',
+    wsgi_script_source          => $::cinder::params::cinder_wsgi_script_source,
+    custom_wsgi_process_options => $custom_wsgi_process_options,
+    require                     => Anchor['cinder::install::end'],
   }
 }
