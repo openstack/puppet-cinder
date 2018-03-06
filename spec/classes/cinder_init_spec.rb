bec1827866f24ad1934b94e8722c7aa3ccf21c7d
Alan Bishop <abishop@redhat.com>
2018-03-06 08:46:19 -0500

    Fix location where backend_host is configured
    When the backend_host option is specified, configure it in each
    cinder-volume backend driver's section and not in the DEFAULT section.
    Backend drivers only look for the option in their backend section, so
    setting in the DEFAULT section had no effect.
    
    Clarify the fact that the DEFAULT host option is *not* deprecated, and
    is only deprecated in backend sections (where the backed_host should be
    used).
    
    Partial-Bug: #1753596
    Change-Id: I11a55571f4bed630967242c797e08e11c47eab11

--- spec/classes/cinder_init_spec.rb
+++ spec/classes/cinder_init_spec.rb
@@ -53,8 +53,10 @@ describe 'cinder' do
       is_expected.to contain_cinder_config('DEFAULT/default_availability_zone').with(:value => 'nova')
       is_expected.to contain_cinder_config('DEFAULT/allow_availability_zone_fallback').with(:value => '<SERVICE DEFAULT>')
       is_expected.to contain_cinder_config('DEFAULT/api_paste_config').with(:value => '/etc/cinder/api-paste.ini')
-      is_expected.to contain_cinder_config('DEFAULT/backend_host').with(:value => '<SERVICE DEFAULT>')
       is_expected.to contain_cinder_config('oslo_concurrency/lock_path').with(:value => '/var/lock/cinder')
+
+      # backend_host should not be written to DEFAULT section
+      is_expected.to_not contain_cinder_config('DEFAULT/backend_host')
     end
 
   end
@@ -286,16 +288,6 @@ describe 'cinder' do
     end
 
     it { is_expected.to contain_cinder_config('DEFAULT/transport_url').with_value('rabbit://rabbit_user:password@localhost:5673') }
-  end
-
-  describe 'with backend_host' do
-    let :params do
-      req_params.merge({
-        :backend_host => 'cinder_backend',
-      })
-    end
-
-    it { is_expected.to contain_cinder_config('DEFAULT/backend_host').with_value('cinder_backend') }
   end
 
 end
