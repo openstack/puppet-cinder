9cc26966c881daad3e77618e2d39f69d504885af
ZhongShengping <chdzsp@163.com>
2018-03-22 11:09:50 +0800

    Add pool_timeout option
    Add pool_timeout option to configure this value for pool_timeout with
    SQLAlchemy.
    
    Change-Id: Ia72d0e2cc2715745f33e71f0b448d84fcdad8f35
    Closes-Bug: #1757581

--- spec/classes/cinder_db_spec.rb
+++ spec/classes/cinder_db_spec.rb
@@ -15,6 +15,7 @@ describe 'cinder::db' do
         :max_retries    => '<SERVICE DEFAULT>',
         :retry_interval => '<SERVICE DEFAULT>',
         :max_overflow   => '<SERVICE DEFAULT>',
+        :pool_timeout   => '<SERVICE DEFAULT>',
       )}
 
     end
@@ -28,7 +29,8 @@ describe 'cinder::db' do
           :database_max_pool_size  => '11',
           :database_max_retries    => '11',
           :database_retry_interval => '11',
-          :database_max_overflow   => '21', }
+          :database_max_overflow   => '21',
+          :database_pool_timeout   => '21', }
       end
 
       it { is_expected.to contain_oslo__db('cinder_config').with(
@@ -40,6 +42,7 @@ describe 'cinder::db' do
         :max_retries    => '11',
         :retry_interval => '11',
         :max_overflow   => '21',
+        :pool_timeout   => '21',
       )}
     end
 
