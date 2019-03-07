bd5f65919b93ba37d9914c5b32cd382e2ca9d04f
Takashi Kajinami <tkajinam@redhat.com>
2019-03-07 10:38:54 +0900

    Add sleep in cinder cron jobs
    The current cron job for purge is executed at the precisely same time,
    if we deploy multiple controller nodes with database clusterd,
    for example using Galera cluster, it could cause DB spike.
    
    This patch introduces a randomized sleep before executing purge job,
    to avoid such kind of collision.
    
    This commit is a kind of cherry-pick of the commit in puppet-nova
    with change id I093a713a4f0ba48686de615aa8cb22b17a56917b .
    
    Co-Authored-By: Rajesh Tailor <ratailor@redhat.com>
    
    Change-Id: I36cf5f1e66fed580786970d9a953d983539a6e43
    (cherry picked from commit f4c9e359fa77d32fa4ed61cc81b58cfe18c5e1ad)

--- spec/classes/cinder_cron_db_purge_spec.rb
+++ spec/classes/cinder_cron_db_purge_spec.rb
@@ -10,6 +10,7 @@ describe 'cinder::cron::db_purge' do
       :weekday     => '*',
       :user        => 'cinder',
       :age         => '30',
+      :maxdelay    => 0,
       :destination => '/var/log/cinder/cinder-rowsflush.log'
     }
   end
@@ -18,6 +19,26 @@ describe 'cinder::cron::db_purge' do
     context 'with required parameters' do
       it { is_expected.to contain_cron('cinder-manage db purge').with(
         :command     => "cinder-manage db purge #{params[:age]} >>#{params[:destination]} 2>&1",
+        :environment => 'PATH=/bin:/usr/bin:/usr/sbin SHELL=/bin/sh',
+        :user        => params[:user],
+        :minute      => params[:minute],
+        :hour        => params[:hour],
+        :monthday    => params[:monthday],
+        :month       => params[:month],
+        :weekday     => params[:weekday],
+        :require     => 'Anchor[cinder::install::end]'
+      )}
+    end
+
+    context 'with required parameters with max delay enabled' do
+      before :each do
+        params.merge!(
+          :maxdelay => 600
+        )
+      end
+
+      it { should contain_cron('cinder-manage db purge').with(
+        :command     => "sleep `expr ${RANDOM} \\% #{params[:maxdelay]}`; cinder-manage db purge #{params[:age]} >>#{params[:destination]} 2>&1",
         :environment => 'PATH=/bin:/usr/bin:/usr/sbin SHELL=/bin/sh',
         :user        => params[:user],
         :minute      => params[:minute],
