openstack/puppet-barbican

Deprecate idle_timeout option
The idle_timeout parameter is deprecated, use connection_recycle_time
instead[1].

[1]https://review.opendev.org/#/c/334182/

Change-Id: I3e6de32bae1896207ff697e6f2fb7cd3e99174e6
Depends-On: https://review.opendev.org/656106/
Closes-Bug: #1826692

manifests/db.pp
@@ -8,7 +8,7 @@
 #   Url used to connect to database.
 #   (Optional) Defaults to "sqlite:////var/lib/barbican/barbican.sqlite".
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   Timeout when db connections should be reaped.
 #   (Optional) Defaults to $::os_service_default
 #
@@ -52,40 +52,54 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class barbican::db (
-  $database_connection     = 'sqlite:////var/lib/barbican/barbican.sqlite',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_size      = $::os_service_default,
-  $database_db_max_retries = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
+  $database_connection              = 'sqlite:////var/lib/barbican/barbican.sqlite',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_size               = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::barbican::deps
+
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+  $database_connection_recycle_time_real = pick($database_idle_timeout, $database_connection_recycle_time)
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection,
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'barbican_config':
-    connection     => $database_connection,
-    idle_timeout   => $database_idle_timeout,
-    min_pool_size  => $database_min_pool_size,
-    max_pool_size  => $database_max_pool_size,
-    max_retries    => $database_max_retries,
-    retry_interval => $database_retry_interval,
-    max_overflow   => $database_max_overflow,
-    db_max_retries => $database_db_max_retries,
-    pool_timeout   => $database_pool_timeout,
+    connection              => $database_connection,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size,
+    max_pool_size           => $database_max_pool_size,
+    max_retries             => $database_max_retries,
+    retry_interval          => $database_retry_interval,
+    max_overflow            => $database_max_overflow,
+    db_max_retries          => $database_db_max_retries,
+    pool_timeout            => $database_pool_timeout,
   }
 
   # TODO(aschultz): Remove this config once barbican properly leverages oslo
   barbican_config {
     'DEFAULT/sql_connection':        value => $database_connection, secret => true;
-    'DEFAULT/sql_idle_timeout':      value => $database_idle_timeout;
+    'DEFAULT/sql_idle_timeout':      value => $database_connection_recycle_time_real;
     'DEFAULT/sql_pool_size':         value => $database_pool_size;
     'DEFAULT/sql_pool_max_overflow': value => $database_max_overflow;
   }

openstack/puppet-cinder

Change-Id: I71d8829b17007a56d1310e401def7894f33f0e14

manifests/db.pp
@@ -13,7 +13,7 @@
 #   Url used to connect to database.
 #   (Optional) Defaults to 'sqlite:////var/lib/cinder/cinder.sqlite'
 #
-# [*database_idle_timeout*]
+# [*database_connection_recycle_time*]
 #   Timeout when db connections should be reaped.
 #   (Optional) Defaults to $::os_service_default
 #
@@ -42,42 +42,55 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
+# DEPRECATED PARAMETERS
+#
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   Defaults to undef.
+#
 class cinder::db (
-  $database_db_max_retries = $::os_service_default,
-  $database_connection     = 'sqlite:////var/lib/cinder/cinder.sqlite',
-  $database_idle_timeout   = $::os_service_default,
-  $database_min_pool_size  = $::os_service_default,
-  $database_max_pool_size  = $::os_service_default,
-  $database_max_retries    = $::os_service_default,
-  $database_retry_interval = $::os_service_default,
-  $database_max_overflow   = $::os_service_default,
-  $database_pool_timeout   = $::os_service_default,
+  $database_db_max_retries          = $::os_service_default,
+  $database_connection              = 'sqlite:////var/lib/cinder/cinder.sqlite',
+  $database_connection_recycle_time = $::os_service_default,
+  $database_min_pool_size           = $::os_service_default,
+  $database_max_pool_size           = $::os_service_default,
+  $database_max_retries             = $::os_service_default,
+  $database_retry_interval          = $::os_service_default,
+  $database_max_overflow            = $::os_service_default,
+  $database_pool_timeout            = $::os_service_default,
+  # DEPRECATED PARAMETERS
+  $database_idle_timeout            = undef,
 ) {
 
   include ::cinder::deps
 
+  if $database_idle_timeout {
+    warning('The database_idle_timeout parameter is deprecated. Please use \
+database_connection_recycle_time instead.')
+  }
+
   # NOTE(spredzy): In order to keep backward compatibility we rely on the pick function
   # to use cinder::<myparam> if cinder::db::<myparam> isn't specified.
-  $database_connection_real     = pick($::cinder::database_connection,$database_connection)
-  $database_idle_timeout_real   = pick($::cinder::database_idle_timeout,$database_idle_timeout)
-  $database_min_pool_size_real  = pick($::cinder::database_min_pool_size,$database_min_pool_size)
-  $database_max_pool_size_real  = pick($::cinder::database_max_pool_size,$database_max_pool_size)
-  $database_max_retries_real    = pick($::cinder::database_max_retries,$database_max_retries)
-  $database_retry_interval_real = pick($::cinder::database_retry_interval,$database_retry_interval)
-  $database_max_overflow_real   = pick($::cinder::database_max_overflow,$database_max_overflow)
+  $database_connection_real              = pick($::cinder::database_connection,$database_connection)
+  $database_connection_recycle_time_real = pick($::cinder::database_idle_timeout, $database_idle_timeout, $database_connection_recycle_time)
+  $database_min_pool_size_real           = pick($::cinder::database_min_pool_size,$database_min_pool_size)
+  $database_max_pool_size_real           = pick($::cinder::database_max_pool_size,$database_max_pool_size)
+  $database_max_retries_real             = pick($::cinder::database_max_retries,$database_max_retries)
+  $database_retry_interval_real          = pick($::cinder::database_retry_interval,$database_retry_interval)
+  $database_max_overflow_real            = pick($::cinder::database_max_overflow,$database_max_overflow)
 
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection_real,
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
 
   oslo::db { 'cinder_config':
-    db_max_retries => $database_db_max_retries,
-    connection     => $database_connection_real,
-    idle_timeout   => $database_idle_timeout_real,
-    min_pool_size  => $database_min_pool_size_real,
-    max_pool_size  => $database_max_pool_size_real,
-    max_retries    => $database_max_retries_real,
-    retry_interval => $database_retry_interval_real,
-    max_overflow   => $database_max_overflow_real,
-    pool_timeout   => $database_pool_timeout,
+    db_max_retries          => $database_db_max_retries,
+    connection              => $database_connection_real,
+    connection_recycle_time => $database_connection_recycle_time_real,
+    min_pool_size           => $database_min_pool_size_real,
+    max_pool_size           => $database_max_pool_size_real,
+    max_retries             => $database_max_retries_real,
+    retry_interval          => $database_retry_interval_real,
+    max_overflow            => $database_max_overflow_real,
+    pool_timeout            => $database_pool_timeout,
   }
 }