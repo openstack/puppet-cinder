c978ba07e77bb347fe88e9219438a117a9d3d1b6
Takashi Kajinami <tkajinam@redhat.com>
2019-06-21 10:12:14 +0900

    Implement class to configure parameters to use service token
    This patch introduces cinder::keystone::service_user class
    to configure parameters to enable service token feature
    in Cinder.
    
    Depends-on: https://review.opendev.org/#/c/666467
    Change-Id: I79362b30107826e67f0bf6dce9371a24bc3ef2dd

---
+++ manifests/keystone/service_user.pp
@@ -0,0 +1,101 @@
+# class: cinder::keystone::service_user
+#
+# Configure the service_user section in the configuration file
+#
+# === Parameters
+#
+# [*username*]
+#   (Optional) The name of the service user
+#   Defaults to 'cinder'
+#
+# [*password*]
+#   (Optional) Password to create for the service user
+#   Defaults to $::os_service_default
+#
+# [*auth_url*]
+#   (Optional) The URL to use for authentication.
+#   Defaults to 'http://localhost:5000'.
+#
+# [*project_name*]
+#   (Optional) Service project name
+#   Defaults to 'services'
+#
+# [*user_domain_name*]
+#   (Optional) Name of domain for $username
+#   Defaults to 'Default'
+#
+# [*project_domain_name*]
+#   (Optional) Name of domain for $project_name
+#   Defaults to 'Default'
+#
+# [*send_service_user_token*]
+#   (Optional) The service uses service token feature when this is set as true
+#   Defaults to 'false'
+#
+# [*insecure*]
+#  (Optional) If true, explicitly allow TLS without checking server cert
+#  against any certificate authorities.  WARNING: not recommended.  Use with
+#  caution.
+#  Defaults to $::os_service_default
+#
+# [*auth_type*]
+#  (Optional) Authentication type to load
+#  Defaults to 'password'
+#
+# [*auth_version*]
+#  (Optional) API version of the admin Identity API endpoint.
+#  Defaults to $::os_service_default.
+#
+# [*cafile*]
+#  (Optional) A PEM encoded Certificate Authority to use when verifying HTTPs
+#  connections.
+#  Defaults to $::os_service_default.
+#
+# [*certfile*]
+#  (Optional) Required if identity server requires client certificate
+#  Defaults to $::os_service_default.
+#
+# [*keyfile*]
+#  (Optional) Required if identity server requires client certificate
+#  Defaults to $::os_service_default.
+#
+# [*region_name*]
+#  (Optional) The region in which the identity server can be found.
+#  Defaults to $::os_service_default.
+#
+class cinder::keystone::service_user(
+  $username                = 'cinder',
+  $password                = $::os_service_default,
+  $auth_url                = 'http://localhost:5000',
+  $project_name            = 'services',
+  $user_domain_name        = 'Default',
+  $project_domain_name     = 'Default',
+  $send_service_user_token = false,
+  $insecure                = $::os_service_default,
+  $auth_type               = 'password',
+  $auth_version            = $::os_service_default,
+  $cafile                  = $::os_service_default,
+  $certfile                = $::os_service_default,
+  $keyfile                 = $::os_service_default,
+  $region_name             = $::os_service_default,
+) {
+
+  include ::cinder::deps
+
+  keystone::resource::service_user { 'cinder_config':
+    username                => $username,
+    password                => $password,
+    project_name            => $project_name,
+    auth_url                => $auth_url,
+    auth_version            => $auth_version,
+    auth_type               => $auth_type,
+    user_domain_name        => $user_domain_name,
+    project_domain_name     => $project_domain_name,
+    send_service_user_token => $send_service_user_token,
+    insecure                => $insecure,
+    cafile                  => $cafile,
+    certfile                => $certfile,
+    keyfile                 => $keyfile,
+    region_name             => $region_name,
+  }
+}
