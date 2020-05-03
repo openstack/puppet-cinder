e67e26e27ad5b62bd1440bb2694da2def3799f58
Takashi Kajinami <tkajinam@redhat.com>
2020-05-04 01:11:06 +0900

    Add common test cases for cinder_config resource
    Change-Id: I19fa59b387079db43e5268acc7a8934ea3120bb3

--- spec/unit/type/cinder_api_paste_ini_spec.rb
+++ spec/unit/type/cinder_api_paste_ini_spec.rb
@@ -1,21 +1,11 @@
-require 'spec_helper'
-# this hack is required for now to ensure that the path is set up correctly
-# to retrieve the parent provider
-$LOAD_PATH.push(
-  File.join(
-    File.dirname(__FILE__),
-    '..',
-    '..',
-    'fixtures',
-    'modules',
-    'inifile',
-    'lib')
-)
+require 'puppet'
 require 'puppet/type/cinder_api_paste_ini'
+
 describe 'Puppet::Type.type(:cinder_api_paste_ini)' do
   before :each do
     @cinder_api_paste_ini = Puppet::Type.type(:cinder_api_paste_ini).new(:name => 'DEFAULT/foo', :value => 'bar')
   end
+
   it 'should accept a valid value' do
     @cinder_api_paste_ini[:value] = 'bar'
     expect(@cinder_api_paste_ini[:value]).to eq('bar')
