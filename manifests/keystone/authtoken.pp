1c680ae8f1314431682674631ccad49bf9679d62
Takashi Kajinami <tkajinam@redhat.com>
2020-07-08 10:52:42 +0900

    Add support for the interface parameter in authtoken middleware
    This patch adds support for [keystone_authtoken] interface parameter,
    so that operators can define which endpoint should be used by authtoken
    middleware.
    
    Change-Id: Ic65f385e5f867cb7346b8b5fc62e8cbf7be8cfb3
    (cherry picked from commit 17b97ec245fb691bfd13b7614012a6ce99d4e204)

--- manifests/keystone/authtoken.pp
+++ manifests/keystone/authtoken.pp
@@ -181,6 +181,11 @@
 #   true/false
 #   Defaults to $::os_service_default.
 #
+# [*interface*]
+#  (Optional) Interface to use for the Identity API endpoint. Valid values are
+#  "public", "internal" or "admin".
+#  Defaults to $::os_service_default.
+#
 class cinder::keystone::authtoken(
   $username                       = 'cinder',
   $password                       = $::os_service_default,
@@ -216,6 +221,7 @@ class cinder::keystone::authtoken(
   $token_cache_time               = $::os_service_default,
   $service_token_roles            = $::os_service_default,
   $service_token_roles_required   = $::os_service_default,
+  $interface                      = $::os_service_default,
 ) {
 
   include cinder::deps
@@ -259,5 +265,6 @@ class cinder::keystone::authtoken(
     token_cache_time               => $token_cache_time,
     service_token_roles            => $service_token_roles,
     service_token_roles_required   => $service_token_roles_required,
+    interface                      => $interface,
   }
 }
