e13e0985807a4bfed1bc9d28410d7dd0a88674a1
rajinir <rajini.karthik@dell.com>
2020-07-20 16:12:53 -0500

    Support for Dell EMC PowerStore Cinder Backend
    Adding support for PowerStore Volume Backend Driver
    
    Depends-On: https://review.opendev.org/#/c/741158/
    Change-Id: I520af0a537236234347ad74b15cd51d72fcec05d

---
+++ manifests/backend/dellemc_powerstore.pp
@@ -0,0 +1,95 @@
+# == define: cinder::backend::dellemc_powerstore
+#
+# Configure the Dell EMC PowerStore Driver for cinder.
+#
+# === Parameters
+#
+# [*san_ip*]
+#   (required) PowerStore REST IP
+#
+# [*san_login*]
+#   (required) PowerStore REST username
+#
+# [*san_password*]
+#   (required) PowerStore REST password
+#
+# [*powerstore_appliances*]
+#   (required) PowerStore appliances
+#
+# [*powerstore_ports*]
+#   (optional) PowerStore allowed ports
+#
+# [*storage_protocol*]
+#   (optional) The Storage protocol, iSCSI or FC.
+#   Defaults to 'iSCSI'
+#
+# [*volume_backend_name*]
+#   (optional) The storage backend name.
+#   Defaults to the name of the backend
+#
+# [*backend_availability_zone*]
+#   (Optional) Availability zone for this volume backend.
+#   If not set, the storage_availability_zone option value
+#   is used as the default for all backends.
+#   Defaults to $::os_service_default.
+#
+# [*manage_volume_type*]
+#   (Optional) Whether or not manage Cinder Volume type.
+#   If set to true, a Cinder Volume type will be created
+#   with volume_backend_name=$volume_backend_name key/value.
+#   Defaults to false.
+#
+# [*extra_options*]
+#   (optional) Hash of extra options to pass to the backend stanza.
+#   Defaults to: {}
+#   Example:
+#     { 'dellemc_powerstore_backend/param1' => { 'value' => value1 } }
+#
+define cinder::backend::dellemc_powerstore (
+  $san_ip,
+  $san_login,
+  $san_password,
+  $powerstore_appliances,
+  $powerstore_ports            = $::os_service_default,
+  $storage_protocol            = 'iSCSI',
+  $volume_backend_name         = $name,
+  $backend_availability_zone   = $::os_service_default,
+  $manage_volume_type          = false,
+  $extra_options               = {},
+) {
+
+  include cinder::deps
+
+  if $storage_protocol == 'iSCSI' {
+    $driver = 'dell_emc.powerstore.driver.PowerStoreDriver'
+  }
+  elsif $storage_protocol == 'FC' {
+    $driver = 'dell_emc.powerstore.driver.PowerStoreDriver'
+  }
+  else {
+    fail('The cinder::backend::dellemc_powerstore storage_protocol specified is not valid. It should be iSCSI or FC')
+  }
+
+  cinder_config {
+    "${name}/volume_backend_name":       value => $volume_backend_name;
+    "${name}/backend_availability_zone": value => $backend_availability_zone;
+    "${name}/volume_driver":             value => "cinder.volume.drivers.${driver}";
+    "${name}/san_ip":                    value => $san_ip;
+    "${name}/san_login":                 value => $san_login;
+    "${name}/san_password":              value => $san_password, secret => true;
+    "${name}/powerstore_appliances":     value => $powerstore_appliances;
+    "${name}/powerstore_ports":          value => $powerstore_ports;
+    "${name}/storage_protocol":          value => $storage_protocol;
+
+  }
+
+  if $manage_volume_type {
+    cinder_type { $volume_backend_name:
+      ensure     => present,
+      properties => ["volume_backend_name=${volume_backend_name}"],
+    }
+  }
+
+  create_resources('cinder_config', $extra_options)
+
+}
---
+++ spec/defines/cinder_backend_dellemc_powerstore_spec.rb
@@ -0,0 +1,123 @@
+require 'spec_helper'
+
+describe 'cinder::backend::dellemc_powerstore' do
+  let (:config_group_name) { 'dellemc_powerstore' }
+
+  let (:title) { config_group_name }
+
+  let :params do
+    {
+      :san_ip                => '172.23.8.101',
+      :san_login             => 'Admin',
+      :san_password          => '12345',
+      :powerstore_appliances => 'Appliance-1',
+
+    }
+  end
+
+  let :default_params do
+    {
+      :powerstore_ports          => '<SERVICE DEFAULT>',
+      :backend_availability_zone => '<SERVICE DEFAULT>',
+      :storage_protocol          => 'iSCSI' ,
+    }
+  end
+
+  let :custom_params do
+    {
+      :powerstore_ports          => '58:cc:f0:98:49:22:07:02,58:cc:f0:98:49:23:07:02',
+      :backend_availability_zone => 'my_zone',
+      :storage_protocol          => 'iSCSI' ,
+    }
+  end
+
+  shared_examples 'dellemc_powerstore volume driver' do
+
+    context 'with default parameters' do
+      let :params_hash do
+        default_params.merge(params)
+      end
+
+      it {
+        is_expected.to contain_cinder__backend__dellemc_powerstore(config_group_name)
+        is_expected.to contain_cinder_config("#{title}/volume_driver").with_value('cinder.volume.drivers.dell_emc.powerstore.driver.PowerStoreDriver')
+      }
+
+      it {
+        params_hash.each_pair do |config,value|
+          is_expected.to contain_cinder_config("#{config_group_name}/#{config}").with_value( value )
+        end
+      }
+    end
+
+    context 'with storage_protocol set to FC' do
+      before do
+        params.merge!(:storage_protocol => 'FC',)
+      end
+
+      it 'should configure the FC driver' do
+        is_expected.to contain_cinder_config("#{title}/volume_driver").with_value(
+          'cinder.volume.drivers.dell_emc.powerstore.driver.PowerStoreDriver'
+        )
+      end
+    end
+
+    context 'with an invalid storage_protocol' do
+      before do
+        params.merge!(:storage_protocol => 'BAD',)
+      end
+
+      it 'should raise an error' do
+        is_expected.to compile.and_raise_error(
+          /The cinder::backend::dellemc_powerstore storage_protocol specified is not valid. It should be iSCSI or FC/
+        )
+      end
+    end
+  end
+
+  shared_examples 'cinder::backend::dellemc_powerstore' do
+
+    context 'with default parameters' do
+      it_behaves_like 'dellemc_powerstore volume driver'
+    end
+
+    context 'with custom parameters' do
+      before do
+        params.merge(custom_params)
+      end
+
+      it_behaves_like 'dellemc_powerstore volume driver'
+    end
+
+    context 'dellemc_powerstore backend with additional configuration' do
+      before do
+        params.merge!( :extra_options => {'dellemc_powerstore/param1' => { 'value' => 'value1' }} )
+      end
+
+      it { is_expected.to contain_cinder_config('dellemc_powerstore/param1').with_value('value1') }
+    end
+
+    context 'dellemc_powerstore backend with cinder type' do
+      before do
+        params.merge!({:manage_volume_type => true})
+      end
+
+      it { is_expected.to contain_cinder_type('dellemc_powerstore').with(
+        :ensure => 'present',
+        :properties => ['volume_backend_name=dellemc_powerstore']
+      )}
+    end
+  end
+
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'cinder::backend::dellemc_powerstore'
+    end
+  end
+end
