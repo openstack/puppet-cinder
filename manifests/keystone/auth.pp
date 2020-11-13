3cb961733b212773145ba93aa955c196d8dc6ed1
Takashi Kajinami <tkajinam@redhat.com>
2020-11-13 15:42:59 +0900

    Deprecate support for v2 API
    This patch deprecates support for v2 API and makes the keystone
    resources for v2 API no longer managed by puppet-cinder, because v2 API
    was deprected a while ago[1] and is supposed to be removed in this
    W-cycle[2].
    
    Note that this change doesn't implement cleanup for v2 API resources,
    and all existing keystone resources like endpoints, service and so on
    should be manually deleted during upgrade.
    
    [1] f6d3454f608ec40570deb62997ccda8048f6e2dc
    [2] http://lists.openstack.org/pipermail/openstack-discuss/2020-November/018531.html
    
    Depends-on: https://review.opendev.org/768178/
    Change-Id: I26a8d679d31a929234b6ebc73035b18f3d65161c

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -11,14 +11,6 @@
 #   (Optional) Email for Cinder user.
 #   Defaults to 'cinder@localhost'.
 #
-# [*password_user_v2*]
-#   (Optional) Password for Cinder v2 user.
-#   Defaults to undef.
-#
-# [*email_user_v2*]
-#   (Optional) Email for Cinder v2 user.
-#   Defaults to 'cinderv2@localhost'.
-#
 # [*password_user_v3*]
 #   (Optional) Password for Cinder v3 user.
 #   Defaults to undef.
@@ -31,17 +23,9 @@
 #   (Optional) Username for Cinder service.
 #   Defaults to 'cinder'.
 #
-# [*auth_name_v2*]
-#   (Optional) Username for Cinder v2 service.
-#   Defaults to 'cinderv2'.
-#
 # [*auth_name_v3*]
 #   (Optional) Username for Cinder v3 service.
 #   Defaults to 'cinderv3'.
-#
-# [*configure_endpoint_v2*]
-#   (Optional) Should Cinder v2 endpoint be configured?
-#   Defaults to true
 #
 # [*configure_endpoint_v3*]
 #   (Optional) Should Cinder v3 endpoint be configured?
@@ -51,10 +35,6 @@
 #   (Optional) Should the service user be configured?
 #   Defaults to true
 #
-# [*configure_user_v2*]
-#   (Optional) Should the service user be configured for cinder v2?
-#   Defaults to false
-#
 # [*configure_user_v3*]
 #   (Optional) Should the service user be configured for cinder v3?
 #   Defaults to false
@@ -63,33 +43,17 @@
 #   (Optional) Should the admin role be configured for the service user?
 #   Defaults to true
 #
-# [*configure_user_role_v2*]
-#   (Optional) Should the admin role be configured for the service user for cinder v2?
-#   Defaults to false
-#
 # [*configure_user_role_v3*]
 #   (Optional) Should the admin role be configured for the service user for cinder v3?
 #   Defaults to false
-#
-# [*service_name_v2*]
-#   (Optional) Name of the v2 service.
-#   Defaults to 'cinderv2'.
 #
 # [*service_name_v3*]
 #   (Optional) Name of the v3 service.
 #   Defaults to 'cinderv3'.
 #
-# [*service_type_v2*]
-#   (Optional) Type of API v2 service.
-#   Defaults to 'volumev2'.
-#
 # [*service_type_v3*]
 #   (Optional) Type of API v3 service.
 #   Defaults to 'volumev3'.
-#
-# [*service_description_v2*]
-#   (Optional) Description for keystone v2 service.
-#   Defaults to 'Cinder Service v2'.
 #
 # [*service_description_v3*]
 #   (Optional) Description for keystone v3 service.
@@ -103,10 +67,6 @@
 #   (Optional) Tenant for Cinder user.
 #   Defaults to 'services'.
 #
-# [*tenant_user_v2*]
-#   (Optional) Tenant for Cinder v2 user.
-#   Defaults to 'services'.
-#
 # [*tenant_user_v3*]
 #   (Optional) Tenant for Cinder v3 user.
 #   Defaults to 'services'.
@@ -115,28 +75,9 @@
 #   (Optional) List of roles assigned to Cinder user
 #   Defaults to ['admin']
 #
-# [*roles_v2*]
-#   (Optional) List of roles assigned to Cinder v2 user
-#   Defaults to ['admin']
-#
 # [*roles_v3*]
 #   (Optional) List of roles assigned to Cinder v3 user
 #   Defaults to ['admin']
-#
-# [*public_url_v2*]
-#   (Optional) The v2 endpoint's public url.
-#   This url should *not* contain any trailing '/'.
-#   Defaults to 'http://127.0.0.1:8776/v2/%(tenant_id)s'
-#
-# [*internal_url_v2*]
-#   (Optional) The v2 endpoint's internal url.
-#   This url should *not* contain any trailing '/'.
-#   Defaults to 'http://127.0.0.1:8776/v2/%(tenant_id)s'
-#
-# [*admin_url_v2*]
-#   (Optional) The v2 endpoint's admin url.
-#   This url should *not* contain any trailing '/'.
-#   Defaults to 'http://127.0.0.1:8776/v2/%(tenant_id)s'
 #
 # [*public_url_v3*]
 #   (0ptional) The v3 endpoint's public url.
@@ -153,6 +94,67 @@
 #   This url should *not* contain any trailing '/'.
 #   Defaults to 'http://127.0.0.1:8776/v3/%(tenant_id)s'
 #
+# DEPRECATED PARAMETRS
+#
+# [*password_user_v2*]
+#   (Optional) Password for Cinder v2 user.
+#   Defaults to undef.
+#
+# [*email_user_v2*]
+#   (Optional) Email for Cinder v2 user.
+#   Defaults to undef
+#
+# [*auth_name_v2*]
+#   (Optional) Username for Cinder v2 service.
+#   Defaults to undef
+#
+# [*configure_endpoint_v2*]
+#   (Optional) Should Cinder v2 endpoint be configured?
+#   Defaults to undef
+#
+# [*configure_user_v2*]
+#   (Optional) Should the service user be configured for cinder v2?
+#   Defaults to undef
+#
+# [*configure_user_role_v2*]
+#   (Optional) Should the admin role be configured for the service user for cinder v2?
+#   Defaults to undef
+#
+# [*service_name_v2*]
+#   (Optional) Name of the v2 service.
+#   Defaults to undef
+#
+# [*service_type_v2*]
+#   (Optional) Type of API v2 service.
+#   Defaults to undef
+#
+# [*service_description_v2*]
+#   (Optional) Description for keystone v2 service.
+#   Defaults to undef
+#
+# [*tenant_user_v2*]
+#   (Optional) Tenant for Cinder v2 user.
+#   Defaults to undef
+#
+# [*roles_v2*]
+#   (Optional) List of roles assigned to Cinder v2 user
+#   Defaults to undef
+#
+# [*public_url_v2*]
+#   (Optional) The v2 endpoint's public url.
+#   This url should *not* contain any trailing '/'.
+#   Defaults to undef
+#
+# [*internal_url_v2*]
+#   (Optional) The v2 endpoint's internal url.
+#   This url should *not* contain any trailing '/'.
+#   Defaults to undef
+#
+# [*admin_url_v2*]
+#   (Optional) The v2 endpoint's admin url.
+#   This url should *not* contain any trailing '/'.
+#   Defaults to undef
+#
 # === Examples
 #
 #  class { 'cinder::keystone::auth':
@@ -163,54 +165,75 @@
 #
 class cinder::keystone::auth (
   $password,
-  $password_user_v2       = undef,
   $password_user_v3       = undef,
   $auth_name              = 'cinder',
-  $auth_name_v2           = 'cinderv2',
   $auth_name_v3           = 'cinderv3',
   $tenant                 = 'services',
-  $tenant_user_v2         = 'services',
   $tenant_user_v3         = 'services',
   $roles                  = ['admin'],
-  $roles_v2               = ['admin'],
   $roles_v3               = ['admin'],
   $email                  = 'cinder@localhost',
-  $email_user_v2          = 'cinderv2@localhost',
   $email_user_v3          = 'cinderv3@localhost',
-  $public_url_v2          = 'http://127.0.0.1:8776/v2/%(tenant_id)s',
-  $internal_url_v2        = 'http://127.0.0.1:8776/v2/%(tenant_id)s',
-  $admin_url_v2           = 'http://127.0.0.1:8776/v2/%(tenant_id)s',
   $public_url_v3          = 'http://127.0.0.1:8776/v3/%(tenant_id)s',
   $internal_url_v3        = 'http://127.0.0.1:8776/v3/%(tenant_id)s',
   $admin_url_v3           = 'http://127.0.0.1:8776/v3/%(tenant_id)s',
-  $configure_endpoint_v2  = true,
   $configure_endpoint_v3  = true,
   $configure_user         = true,
-  $configure_user_v2      = false,
   $configure_user_v3      = false,
   $configure_user_role    = true,
-  $configure_user_role_v2 = false,
   $configure_user_role_v3 = false,
-  $service_name_v2        = 'cinderv2',
   $service_name_v3        = 'cinderv3',
-  $service_type_v2        = 'volumev2',
   $service_type_v3        = 'volumev3',
-  $service_description_v2 = 'Cinder Service v2',
   $service_description_v3 = 'Cinder Service v3',
   $region                 = 'RegionOne',
+  # DEPRECATED PARAMETERS
+  $password_user_v2       = undef,
+  $auth_name_v2           = undef,
+  $tenant_user_v2         = undef,
+  $roles_v2               = undef,
+  $email_user_v2          = undef,
+  $public_url_v2          = undef,
+  $internal_url_v2        = undef,
+  $admin_url_v2           = undef,
+  $configure_endpoint_v2  = undef,
+  $configure_user_v2      = undef,
+  $configure_user_role_v2 = undef,
+  $service_name_v2        = undef,
+  $service_type_v2        = undef,
+  $service_description_v2 = undef,
 ) {
 
   include cinder::deps
 
-  if $configure_endpoint_v2 {
-    Keystone_endpoint["${region}/${service_name_v2}::${service_type_v2}"] -> Anchor['cinder::service::end']
-  }
   if $configure_endpoint_v3 {
     Keystone_endpoint["${region}/${service_name_v3}::${service_type_v3}"] -> Anchor['cinder::service::end']
   }
 
-  # Always configure the original (non-v2|v3) user and user roles, as these
-  # can be used by the v2 and v3 services.
+  $deprecated_v2_param_names = [
+    'password_user_v2',
+    'auth_name_v2',
+    'tenant_user_v2',
+    'roles_v2',
+    'email_user_v2',
+    'public_url_v2',
+    'internal_url_v2',
+    'admin_url_v2',
+    'configure_endpoint_v2',
+    'configure_user_v2',
+    'configure_user_role_v2',
+    'service_name_v2',
+    'service_type_v2',
+    'service_description_v2',
+  ]
+  $deprecated_v2_param_names.each |$param_name| {
+    $param = getvar($param_name)
+    if $param != undef{
+      warning("The ${param_name} parameter is deprecated, has no effect and will be removed in a future release.")
+    }
+  }
+
+  # Always configure the original user and user roles, as these
+  # can be used by the v3 service.
   keystone::resource::service_identity { 'cinder':
     configure_user      => $configure_user,
     configure_user_role => $configure_user_role,
@@ -222,24 +245,6 @@ class cinder::keystone::auth (
     email               => $email,
     tenant              => $tenant,
     roles               => $roles,
-  }
-
-  keystone::resource::service_identity { 'cinderv2':
-    configure_user      => $configure_user_v2,
-    configure_user_role => $configure_user_role_v2,
-    configure_endpoint  => $configure_endpoint_v2,
-    service_type        => $service_type_v2,
-    service_description => $service_description_v2,
-    service_name        => $service_name_v2,
-    region              => $region,
-    auth_name           => $auth_name_v2,
-    password            => $password_user_v2,
-    email               => $email_user_v2,
-    tenant              => $tenant_user_v2,
-    roles               => $roles_v2,
-    public_url          => $public_url_v2,
-    admin_url           => $admin_url_v2,
-    internal_url        => $internal_url_v2,
   }
 
   keystone::resource::service_identity { 'cinderv3':
--- spec/classes/cinder_keystone_auth_spec.rb
+++ spec/classes/cinder_keystone_auth_spec.rb
@@ -20,21 +20,9 @@ describe 'cinder::keystone::auth' do
         :roles   => ['admin']
       )}
 
-      it { is_expected.to contain_keystone_service('cinderv2::volumev2').with(
-        :ensure      => 'present',
-        :description => 'Cinder Service v2'
-      )}
-
       it { is_expected.to contain_keystone_service('cinderv3::volumev3').with(
         :ensure      => 'present',
         :description => 'Cinder Service v3'
-      )}
-
-      it { is_expected.to contain_keystone_endpoint('RegionOne/cinderv2::volumev2').with(
-        :ensure       => 'present',
-        :public_url   => 'http://127.0.0.1:8776/v2/%(tenant_id)s',
-        :admin_url    => 'http://127.0.0.1:8776/v2/%(tenant_id)s',
-        :internal_url => 'http://127.0.0.1:8776/v2/%(tenant_id)s'
       )}
 
       it { is_expected.to contain_keystone_endpoint('RegionOne/cinderv3::volumev3').with(
@@ -48,27 +36,17 @@ describe 'cinder::keystone::auth' do
     context 'when overriding parameters' do
       before do
         params.merge!({
-          :roles           => ['admin', 'service'],
-          :region          => 'RegionThree',
-          :public_url_v2   => 'https://10.0.42.1:4242/v42/%(tenant_id)s',
-          :admin_url_v2    => 'https://10.0.42.2:4242/v42/%(tenant_id)s',
-          :internal_url_v2 => 'https://10.0.42.3:4242/v42/%(tenant_id)s',
-          :public_url_v3   => 'https://10.0.42.1:4242/v43/%(tenant_id)s',
-          :admin_url_v3    => 'https://10.0.42.2:4242/v43/%(tenant_id)s',
-          :internal_url_v3 => 'https://10.0.42.3:4242/v43/%(tenant_id)s'
+          :roles                 => ['admin', 'service'],
+          :region                => 'RegionThree',
+          :public_url_v3         => 'https://10.0.42.1:4242/v43/%(tenant_id)s',
+          :admin_url_v3          => 'https://10.0.42.2:4242/v43/%(tenant_id)s',
+          :internal_url_v3       => 'https://10.0.42.3:4242/v43/%(tenant_id)s'
         })
       end
 
       it { is_expected.to contain_keystone_user_role('cinder@services').with(
         :ensure  => 'present',
         :roles   => ['admin', 'service']
-      )}
-
-      it { is_expected.to contain_keystone_endpoint('RegionThree/cinderv2::volumev2').with(
-        :ensure       => 'present',
-        :public_url   => 'https://10.0.42.1:4242/v42/%(tenant_id)s',
-        :admin_url    => 'https://10.0.42.2:4242/v42/%(tenant_id)s',
-        :internal_url => 'https://10.0.42.3:4242/v42/%(tenant_id)s'
       )}
 
       it { is_expected.to contain_keystone_endpoint('RegionThree/cinderv3::volumev3').with(
@@ -82,12 +60,10 @@ describe 'cinder::keystone::auth' do
     context 'when endpoint should not be configured' do
       before do
         params.merge!(
-          :configure_endpoint_v2 => false,
           :configure_endpoint_v3 => false
         )
       end
 
-      it { is_expected.not_to contain_keystone_endpoint('RegionOne/cinderv2::volumev2') }
       it { is_expected.not_to contain_keystone_endpoint('RegionOne/cinderv3::volumev3') }
     end
 
@@ -124,22 +100,6 @@ describe 'cinder::keystone::auth' do
       )}
     end
 
-    context 'when user and user role for v2 should be configured' do
-      before do
-        params.merge!(
-          :configure_user_v2      => true,
-          :configure_user_role_v2 => true,
-        )
-      end
-
-      it { is_expected.to contain_keystone__resource__service_identity('cinderv2').with(
-        :configure_user      => true,
-        :configure_user_role => true,
-        :email               => 'cinderv2@localhost',
-        :tenant              => 'services'
-      )}
-    end
-
     context 'when user and user role for v3 should be configured' do
       before do
         params.merge!(
@@ -159,16 +119,13 @@ describe 'cinder::keystone::auth' do
     context 'when overriding service names' do
       before do
         params.merge!(
-          :service_name_v2 => 'cinder_service_v2',
           :service_name_v3 => 'cinder_service_v3',
         )
       end
 
       it { is_expected.to contain_keystone_user('cinder') }
       it { is_expected.to contain_keystone_user_role('cinder@services') }
-      it { is_expected.to contain_keystone_service('cinder_service_v2::volumev2') }
       it { is_expected.to contain_keystone_service('cinder_service_v3::volumev3') }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/cinder_service_v2::volumev2') }
       it { is_expected.to contain_keystone_endpoint('RegionOne/cinder_service_v3::volumev3') }
     end
   end
