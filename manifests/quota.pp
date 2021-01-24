03b8d85d7c1dde5a4e8b354076d137e992890907
Takashi Kajinami <tkajinam@redhat.com>
2021-01-24 10:50:53 +0900

    Add support for backup quotas
    Change-Id: Ic4b6acf939b85db53e4ca782a6f29f80cacc2c14

--- manifests/quota.pp
+++ manifests/quota.pp
@@ -17,23 +17,35 @@
 #   allowed per project.
 #   Defaults to $::os_service_default.
 #
+# [*quota_backups*]
+#   (Optional) Number of volume backups allowed per project.
+#   Defaults to $::os_service_default.
+#
+# [*quota_backup_gigabytes*]
+#   (Optional) Number of backup gigabytes allowed per project.
+#   Defaults to $::os_service_default.
+#
 # [*quota_driver*]
 #   (Optional) Default driver to use for quota checks.
 #   Defaults to $::os_service_default.
 #
 class cinder::quota (
-  $quota_volumes   = $::os_service_default,
-  $quota_snapshots = $::os_service_default,
-  $quota_gigabytes = $::os_service_default,
-  $quota_driver    = $::os_service_default,
+  $quota_volumes          = $::os_service_default,
+  $quota_snapshots        = $::os_service_default,
+  $quota_gigabytes        = $::os_service_default,
+  $quota_backups          = $::os_service_default,
+  $quota_backup_gigabytes = $::os_service_default,
+  $quota_driver           = $::os_service_default,
 ) {
 
   include cinder::deps
 
   cinder_config {
-    'DEFAULT/quota_volumes':   value => $quota_volumes;
-    'DEFAULT/quota_snapshots': value => $quota_snapshots;
-    'DEFAULT/quota_gigabytes': value => $quota_gigabytes;
-    'DEFAULT/quota_driver':    value => $quota_driver;
+    'DEFAULT/quota_volumes':          value => $quota_volumes;
+    'DEFAULT/quota_snapshots':        value => $quota_snapshots;
+    'DEFAULT/quota_gigabytes':        value => $quota_gigabytes;
+    'DEFAULT/quota_backups':          value => $quota_backups;
+    'DEFAULT/quota_backup_gigabytes': value => $quota_backup_gigabytes;
+    'DEFAULT/quota_driver':           value => $quota_driver;
   }
 }
