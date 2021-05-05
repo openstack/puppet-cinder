870ff626558c0203a4be1ae64e5d31212c0cd80f
Takashi Kajinami <tkajinam@redhat.com>
2021-05-05 10:43:00 +0900

    Add support for MultiStrOpt
    This replaces the provider implementation of cinder_config type so that
    MultiStrOpt, which is used by several options like
     - oslo_messaging_notifications/driver
     - oslo_policy/policy_dirs
    is handled correctly.
    
    Change-Id: If052cde4da84970df4af657136060532ccb422f7

--- lib/puppet/type/cinder_config.rb
+++ lib/puppet/type/cinder_config.rb
@@ -7,14 +7,22 @@ Puppet::Type.newtype(:cinder_config) do
     newvalues(/\S+\/\S+/)
   end
 
-  newproperty(:value) do
+  newproperty(:value, :array_matching => :all) do
     desc 'The value of the setting to be defined.'
+    def insync?(is)
+      return true if @should.empty?
+      return false unless is.is_a? Array
+      return false unless is.length == @should.length
+      return (
+        is & @should == is or
+        is & @should.map(&:to_s) == is
+      )
+    end
     munge do |value|
       value = value.to_s.strip
       value.capitalize! if value =~ /^(true|false)$/i
       value
     end
-    newvalues(/^[\S ]*$/)
 
     def is_to_s( currentvalue )
       if resource.secret?
@@ -49,4 +57,5 @@ Puppet::Type.newtype(:cinder_config) do
   autorequire(:anchor) do
     ['cinder::install::end']
   end
+
 end
