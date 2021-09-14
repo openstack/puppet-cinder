d20eb43ec8986d35ab5f367a0d8217a046843ae6
ZhongShengping <chdzsp@163.com>
2021-09-14 16:02:08 +0800

    Add watch_log_file option
    Add support for Using logging handler designed to watch file system.
    
    Change-Id: I86fe2ea8facfe5363ffb343978c3db20620599e1
    Closes-Bug: #1943212

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -37,6 +37,10 @@
 #   (Optional) File where logs should be stored.
 #   Defaults to $::os_service_default
 #
+# [*watch_log_file*]
+#   (Optional) Uses logging handler designed to watch file system (boolean value).
+#   Defaults to $::os_service_default
+#
 # [*logging_context_format_string*]
 #   (Optional) Format string to use for log messages with context.
 #   Defaults to $::os_service_default
@@ -105,6 +109,7 @@ class cinder::logging(
   $log_facility                  = $::os_service_default,
   $log_dir                       = '/var/log/cinder',
   $log_file                      = $::os_service_default,
+  $watch_log_file                = $::os_service_default,
   $debug                         = $::os_service_default,
   $logging_context_format_string = $::os_service_default,
   $logging_default_format_string = $::os_service_default,
@@ -129,6 +134,7 @@ class cinder::logging(
     use_stderr                    => $use_stderr,
     log_dir                       => $log_dir,
     log_file                      => $log_file,
+    watch_log_file                => $watch_log_file,
     syslog_log_facility           => $log_facility,
     logging_context_format_string => $logging_context_format_string,
     logging_default_format_string => $logging_default_format_string,
--- spec/classes/cinder_logging_spec.rb
+++ spec/classes/cinder_logging_spec.rb
@@ -29,6 +29,7 @@ describe 'cinder::logging' do
      :log_facility                   => 'LOG_FOO',
      :log_dir                        => '/var/log',
      :log_file                       => '/var/log/cinder/cinder.log',
+     :watch_log_file                 => true,
      :debug                          => true,
     }
   end
@@ -58,13 +59,14 @@ describe 'cinder::logging' do
   shared_examples 'basic default logging settings' do
     it 'configures cinder logging settings with default values' do
       is_expected.to contain_oslo__log('cinder_config').with(
-        :use_syslog  => '<SERVICE DEFAULT>',
-        :use_json    => '<SERVICE DEFAULT>',
-        :use_journal => '<SERVICE DEFAULT>',
-        :use_stderr  => '<SERVICE DEFAULT>',
-        :log_dir     => '/var/log/cinder',
-        :log_file    => '<SERVICE DEFAULT>',
-        :debug       => '<SERVICE DEFAULT>',
+        :use_syslog     => '<SERVICE DEFAULT>',
+        :use_json       => '<SERVICE DEFAULT>',
+        :use_journal    => '<SERVICE DEFAULT>',
+        :use_stderr     => '<SERVICE DEFAULT>',
+        :log_dir        => '/var/log/cinder',
+        :log_file       => '<SERVICE DEFAULT>',
+        :watch_log_file => '<SERVICE DEFAULT>',
+        :debug          => '<SERVICE DEFAULT>',
       )
     end
   end
@@ -79,6 +81,7 @@ describe 'cinder::logging' do
         :syslog_log_facility => 'LOG_FOO',
         :log_dir             => '/var/log',
         :log_file            => '/var/log/cinder/cinder.log',
+        :watch_log_file      => true,
         :debug               => true,
       )
     end
