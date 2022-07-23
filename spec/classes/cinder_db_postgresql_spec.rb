41d313659b36f47b2745274a0753146fc8a33433
Takashi Kajinami <tkajinam@redhat.com>
2022-07-24 00:27:50 +0900

    Revert "CentOS 9: Disable unit tests dependent on puppet-postgresql"
    This reverts commit 6d4ba3441e4bde86c27f25407879183ef9588431.
    
    Reason for revert:
    puppet-postgresql 8.1.0 was released and now the module supports RHEL 9
    (and CentOS 9 effectively).
    
    Note:
    This change adds the service_provider fact in test fact data because
    it is required by puppet-postgresql.
    
    Depends-on: https://review.opendev.org/850705
    Change-Id: I7ec4a54ed165febe5df6b2c05ceeb1285ff04da0

--- spec/classes/cinder_db_postgresql_spec.rb
+++ spec/classes/cinder_db_postgresql_spec.rb
@@ -33,15 +33,13 @@ describe 'cinder::db::postgresql' do
     context "on #{os}" do
       let (:facts) do
         facts.merge(OSDefaults.get_facts({
-          :os_workers => 8,
-          :concat_basedir => '/var/lib/puppet/concat'
+          # puppet-postgresql requires the service_provider fact provided by
+          # puppetlabs-postgresql.
+          :service_provider => 'systemd'
         }))
       end
 
-      # TODO(tkajinam): Remove this once puppet-postgresql supports CentOS 9
-      unless facts[:osfamily] == 'RedHat' and facts[:operatingsystemmajrelease].to_i >= 9
-        it_behaves_like 'cinder::db::postgresql'
-      end
+      it_behaves_like 'cinder::db::postgresql'
     end
   end
 
