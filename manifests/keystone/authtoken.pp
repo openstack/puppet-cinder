f95fa42e812ab213cdc402b7cbd5982288933188
Takashi Kajinami <tkajinam@redhat.com>
2023-06-23 10:24:45 +0900

    authtoken: Make password required
    The password parameter is not really optional. This makes it
    a required parameter to give more sensible validation error.
    
    Change-Id: Id6fc79b9725e9c88e90b06c002ccfde59c75b089

--- manifests/keystone/authtoken.pp
+++ manifests/keystone/authtoken.pp
@@ -4,13 +4,12 @@
 #
 # === Parameters
 #
+# [*password*]
+#   (Required) Password to create for the service user
+#
 # [*username*]
 #   (Optional) The name of the service user
 #   Defaults to 'cinder'
-#
-# [*password*]
-#   (Optional) Password to create for the service user
-#   Defaults to $facts['os_service_default']
 #
 # [*auth_url*]
 #   (Optional) The URL to use for authentication.
@@ -201,8 +200,8 @@
 #  authtoken class. Values set here override the individual parameters above.
 #
 class cinder::keystone::authtoken(
+  String[1] $password,
   $username                       = 'cinder',
-  $password                       = $facts['os_service_default'],
   $auth_url                       = 'http://localhost:5000',
   $project_name                   = 'services',
   $user_domain_name               = 'Default',
@@ -242,10 +241,6 @@ class cinder::keystone::authtoken(
 ) {
 
   include cinder::deps
-
-  if is_service_default($password) {
-    fail('Please set password for cinder service user')
-  }
 
   keystone::resource::authtoken {
     'cinder_config':
