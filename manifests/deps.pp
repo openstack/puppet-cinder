79783fd4bf0734c0794158cd649765c5e77fc677
Takashi Kajinami <tkajinam@redhat.com>
2023-09-11 12:26:15 +0900

    Do not restart services after policy file changes
    The oslo.policy library has implementations to detect change in policy
    rules and reload the new rules without service restart.
    
    Change-Id: I09d8e48e9cb3e2a090bf2b3201c3b47a406f6f83

--- manifests/deps.pp
+++ manifests/deps.pp
@@ -45,7 +45,7 @@ class cinder::deps {
   # policy config should occur in the config block also.
   Anchor['cinder::config::begin']
   -> Openstacklib::Policy<| tag == 'cinder' |>
-  ~> Anchor['cinder::config::end']
+  -> Anchor['cinder::config::end']
 
   # On any uwsgi config change, we must restart Cinder API.
   Anchor['cinder::config::begin']
