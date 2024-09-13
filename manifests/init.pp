8bf8355aed0f141bbd0e3ddfd300809ec573414a
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-13 11:24:40 +0900

    Expose rabbit_qos_prefetch_count
    The option was supported by puppet-oslo but was not configurable via
    this module.
    
    Change-Id: I607cfa83aa9d521461bb0758215015bb9e52c68e

--- manifests/init.pp
+++ manifests/init.pp
@@ -69,6 +69,10 @@
 #   will be run through a green thread.
 #   Defaults to $facts['os_service_default']
 #
+# [*rabbit_qos_prefetch_count*]
+#   (Optional) Specifies the number of messages to prefetch.
+#   Defaults to $facts['os_service_default']
+#
 # [*rabbit_quorum_queue*]
 #   (Optional) Use quorum queues in RabbitMQ.
 #   Defaults to $facts['os_service_default']
@@ -232,6 +236,7 @@ class cinder (
   $rabbit_heartbeat_timeout_threshold   = $facts['os_service_default'],
   $rabbit_heartbeat_rate                = $facts['os_service_default'],
   $rabbit_heartbeat_in_pthread          = $facts['os_service_default'],
+  $rabbit_qos_prefetch_count            = $facts['os_service_default'],
   $rabbit_quorum_queue                  = $facts['os_service_default'],
   $rabbit_transient_quorum_queue        = $facts['os_service_default'],
   $rabbit_quorum_delivery_limit         = $facts['os_service_default'],
@@ -285,6 +290,7 @@ class cinder (
     heartbeat_timeout_threshold     => $rabbit_heartbeat_timeout_threshold,
     heartbeat_rate                  => $rabbit_heartbeat_rate,
     heartbeat_in_pthread            => $rabbit_heartbeat_in_pthread,
+    rabbit_qos_prefetch_count       => $rabbit_qos_prefetch_count,
     rabbit_use_ssl                  => $rabbit_use_ssl,
     kombu_reconnect_delay           => $kombu_reconnect_delay,
     kombu_failover_strategy         => $kombu_failover_strategy,
